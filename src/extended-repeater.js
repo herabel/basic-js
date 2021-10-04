bc03783405dd64ddf67b2e673f6abc1ca0df90d3
herabel <3423.0a@mail.ru>
2021-10-04 21:29:40 +0300

    feat: do 4 more tasks
    (extended repeater, hanoi tower, recursive depth, simple chain)

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,24 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  let res = "";
+  str = String(str)
+  if (options.addition || typeof options.addition === 'boolean' || options.addition === null) options.addition = String(options.addition)
+  if (!options.separator) options.separator = "+";
+  if (!options.additionSeparator) options.additionSeparator = "|";
+  if (!options.repeatTimes) options.repeatTimes = 1;
+  if (!options.additionRepeatTimes) options.additionRepeatTimes = 1;
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+      let add = "";
+      if (options.addition) {
+          for (let i = 0; i < options.additionRepeatTimes; i++) {
+              add += options.addition + options.additionSeparator;
+          }
+          add = add.substring(0, add.length - options.additionSeparator.length);
+      }
+      res += str + add + options.separator;
+  }
+  return res.substring(0, res.length - options.separator.length);
 }
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,7 +14,9 @@ import { NotImplementedError } from '../extensions/index.js';
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-export default function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function calculateHanoi(disksNumber, turnsSpeed) {
+  var turns = Math.pow(2, disksNumber) - 1;
+  var seconds = Math.floor((3600 * turns) / turnsSpeed);
+
+  return { turns: turns, seconds: seconds };
 }
